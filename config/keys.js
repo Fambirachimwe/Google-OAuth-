af51ea517c6eb5492f8479325b24c81a1137f49a
Fambirachimwe <t.svaku@gmail.com>
2019-06-18 09:24:24 +0200

    changes on the root

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: "338113355382-p1pmpgso41ottn1iinpseg7facdecq6s.apps.googleusercontent.com",
-        clientSecret: 'vS91kU8K4637PA341Pdv1-mq',
-    }
-}
